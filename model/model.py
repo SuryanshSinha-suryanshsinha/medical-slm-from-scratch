058ba65dca31a139f43c19a7d68ed8e3cac260c4
SuryanshSinha-suryanshsinha <suryanshsinha.dev@gmail.com>
2026-03-19 10:22:46 +0530

    add modelconfig

--- model/model.py
+++ model/model.py
@@ -20,4 +20,13 @@ class ModelConfig:
     norm_eps:         float = 1e-6     
     max_batch_size:   int   = 4  
 
-     
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        rms = x.pow(2).mean(dim=-1, keepdim=True).add(self.eps).sqrt()
+        return self.weight * (x / rms)
